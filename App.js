cd09ace40a6df24a076a245c1aab095079b9e84d
taimoortn <taimoortn@gmail.com>
2019-11-03 11:38:06 +0500

    added

--- App.js
+++ App.js
@@ -1,27 +1,56 @@
-//import liraries
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
-// create a component
-class MyClass extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>MyClass</Text>
-      </View>
-    );
-  }
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity
+} from "react-native";
+import { createStore } from 'redux'
+import CounterApp from './src/CounterApp'
+import { Provider } from 'react-redux'
+/**
+ * Store - holds our state - THERE IS ONLY ONE STATE 
+ * Action - State can be modified using actions - SIMPLE OBJECTS 
+ * Dispatcher - Action needs to be sent by someone - known as dispatching an action
+ * Reducer - receives the action and modifies the state to give us a new state 
+ *  - pure functions 
+ *  - only mandatory argument is the 'type' 
+ * Subscriber - listens for state change to update the ui  
+ */
+const initialState = {
+    counter: 0
+}
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INCREASE_COUNTER':
+            return { counter: state.counter + 1 }
+        case 'DECREASE_COUNTER':
+            return { counter: state.counter - 1 }
+    }
+    return state
 }
 
-// define your styles
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#2c3e50',
-  },
-});
+const store = createStore(reducer)
 
-//make this component available to the app
-export default MyClass;
+class App extends Component {
+
+    render() {
+        return (
+            <Provider store={store}>
+                <CounterApp />
+            </Provider>
+        );
+    }
+}
+
+export default App
+
+// export default App;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
